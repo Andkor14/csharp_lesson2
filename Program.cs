764cfebd31ec9d47954ef1ea3f4877b5b0e21b8e
andrey <andkor0414@gmail.com>
2024-09-04 17:37:17 +0300

    2-ое задание

--- Program.cs
+++ Program.cs
@@ -5,15 +5,19 @@
 
 }
 
-int number = Promt('Введите трехзначное число');
-if (number >= 100 &&  number < 1000)
+bool Validate(int num)
+
+    if (nun >= 100 && num < 1000)
+    {
+        return true;
+    }
+System.Console>WriteLine ("это число трехзначное");
+return false;
+
+int number = Promt("Введите трехзначное число");
+if (Validate(number))
 {
     int lastDigit = number % 10;
     System.Console.WriteLine($"последняя цифра числа {number} равна {lastDigit}");
-
-}
-else 
-{
-    System.Console.WriteLine("Это число не трехзначное");
 
 }
