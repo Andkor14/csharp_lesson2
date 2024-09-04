a26d6a978dae4260285cf23f638f70457759ba94
andrey <andkor0414@gmail.com>
2024-09-04 17:04:48 +0300

    1-оее задание

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-q
+﻿int Promt(string msg)
+{
+    System.Console.Write($"{msg}>");
+    return Convert.Toint32(Console.ReadLine());
+
+}
+
+int number = Promt('Введите трехзначное число');
+if (number >= 100 &&  number < 1000)
+{
+    int lastDigit = number % 10;
+    System.Console.WriteLine($"последняя цифра числа {number} равна {lastDigit}");
+
+}
+else 
+{
+    System.Console.WriteLine("Это число не трехзначное");
+
+}
